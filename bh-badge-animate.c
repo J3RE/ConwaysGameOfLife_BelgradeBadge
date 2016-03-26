0b66fdccd337e631e079eb77b467741377a1317e
J3RE <j3re@gmx.at>
2016-03-27 00:14:07 +0100

    fixed bugs, added intro screen

--- bh-badge-animate.c
+++ bh-badge-animate.c
@@ -16,17 +16,7 @@ typedef enum
   PAUSE_EDITOR
 } mode_state;
 
-mode_state mode = MENU;
-
-uint8_t BufferBackup[TOTPIXELY];
-
-uint16_t delay_time = 500;
-uint16_t delay_time_scrolling = 100;
-uint32_t time_old;
-int8_t i_x = 0, i_y = 0;
-uint8_t toggle_pixel = 0;
-
-char text[]="Hackaday Belgrade!\0";
+char intro[]="Hackaday Belgrade! \0";
 char char_play[]="play \0";
 char char_editor[]="editor \0";
 
@@ -131,7 +121,6 @@ void refreshMatrix(uint8_t array[])
   {
     Buffer[y] = array[y];
   }
-  displayLatch();
 }
 
 uint8_t readPixel(int8_t x, int8_t y, uint8_t array[])
@@ -139,15 +128,15 @@ uint8_t readPixel(int8_t x, int8_t y, uint8_t array[])
   if(!((x >= 0) && (x < TOTPIXELX) && (y >= 0) && (y < TOTPIXELY)))
     return 0;
 
-  return (array[y] >> x) & 0x01;
+  return (array[y] >> (7 - x)) & 0x01;
 }
 
 void writePixel(uint8_t x, uint8_t y, uint8_t live, uint8_t array[])
 {
   if(live)
-    array[y] |= (1 << x);
+    array[y] |= (1 << (7 - x));
   else
-    array[y] &= ~(1 << x);
+    array[y] &= ~(1 << (7 - x));
 }
 
 void printChar(int8_t x, int8_t y, char character)
@@ -165,9 +154,6 @@ void printChar(int8_t x, int8_t y, char character)
         Buffer[y + i] |= font[i_array + i] >> (x - 3);
     }
   }
-
-  displayLatch();
-
 }
 
 // positive steps scroll left
@@ -189,196 +175,85 @@ void scrollDisplay(uint8_t y_start, uint8_t y_end, int8_t steps)
   }
 }
 
-uint8_t checkButtons()
-{
-  uint8_t key_pressed = getControl();
-
-  switch(mode)
-  {
-    case PAUSE:
-      switch (key_pressed)
-      {
-        case ESCAPE:
-          return 1;
-        case LEFT:
-          mode = MENU;
-          displayClear();
-          displayLatch();
-          return 3;
-          break;
-        case RIGHT:
-          mode = PLAY;
-          break;
-        case UP:
-          return 2;
-          break;
-        case DOWN:
-          mode = PLAY;
-          break;
-      }
-      break;
-
-    case PLAY:
-      switch (key_pressed)
-      {
-        case ESCAPE:
-          return 1;
-        case LEFT:
-          if(delay_time > 50)
-          {
-            delay_time -= 50;
-            displayClear();
-            printChar(2, 4, '-');
-            time_old = getTime();
-          }
-          break;
-        case RIGHT:
-          delay_time += 50;
-          displayClear();
-          printChar(2, 4, '+');
-          time_old = getTime();
-          break;
-        case UP:
-          return 2;
-          break;
-        case DOWN:
-          mode = PAUSE;
-          break;
-      }
-      break;
-
-    case MENU:
-      switch (key_pressed)
-      {
-        case ESCAPE:
-          return 1;
-        case LEFT:
-          delay_time_scrolling -= 10;
-          break;
-        case RIGHT:
-          delay_time_scrolling += 10;
-          break;
-        case UP:
-          mode = PLAY;
-          return 2;
-          break;
-        case DOWN:
-          mode = EDITOR;
-          displayClear();
-          displayLatch();
-          break;
-      }
-      break;
-
-    case EDITOR:
-      switch (key_pressed)
-      {
-        case ESCAPE:
-          return 1;
-        case LEFT:
-          toggle_pixel = 1;
-          break;
-        case RIGHT:
-          if(++i_x == TOTPIXELX)
-          {
-            i_x = 0;
-          }
-          break;
-        case UP:
-          if(!i_x && !i_y)
-          {
-            i_y = -1;
-          }
-          else if(i_y)
-          {
-            i_y--;
-          }
-          break;
-        case DOWN:
-          if(i_y < (TOTPIXELY - 1))
-          {
-            i_y++;
-          }
-          break;
-      }
-      break;
-
-    case PLAY_EDITOR:
-      switch (key_pressed)
-      {
-        case ESCAPE:
-          return 1;
-        case LEFT:
-          if(delay_time > 50)
-          {
-            delay_time -= 50;
-            displayClear();
-            printChar(2, 4, '-');
-            time_old = getTime();
-          }
-          break;
-        case RIGHT:
-          delay_time += 50;
-          displayClear();
-          printChar(2, 4, '+');
-          time_old = getTime();
-          break;
-        case UP:
-          displayClear();
-          for(int i = 0; i < TOTPIXELY; i++)
-          {
-            Buffer[i] = BufferBackup[i];
-          }
-          mode = EDITOR;
-          break;
-        case DOWN:
-          mode = PAUSE_EDITOR;
-          break;
-      }
-      break;
-
-    case PAUSE_EDITOR:
-      switch (key_pressed)
-      {
-        case ESCAPE:
-          return 1;
-        case LEFT:
-          mode = MENU;
-          displayClear();
-          displayLatch();
-          return 3;
-          break;
-        case RIGHT:
-          mode = PLAY_EDITOR;
-          break;
-        case UP:
-          return 2;
-          break;
-        case DOWN:
-          mode = PLAY;
-          break;
-      }
-      break;
-  }
-
-  return 0;
-}
-
 void animateBadge(void)
 { 
-  uint8_t playground[TOTPIXELY];
-  uint8_t pause_state = 0;
-  uint8_t i_char1 = 0, i_char2 = 0;
-  uint8_t x = 0, y = 0;
+  mode_state mode = MENU;
 
-  restart(playground);
+  uint8_t playground[TOTPIXELY];
+  uint8_t BufferEditor[TOTPIXELY] = {};
+  
+  uint8_t pause_state = 0, blink_editor = 0;
+  uint8_t i_char1 = 0, i_char2 = 0;
+  int8_t i_x = 0, i_y = 0;
+  uint32_t time_old;
+  uint16_t delay_time = 500;
+  uint16_t delay_time_scrolling = 100;
+
+  // add a smiley, Hackaday logo is to complex
+  Buffer[ 8] = 0b00111100;
+  Buffer[ 9] = 0b01000010;
+  Buffer[10] = 0b10100101;
+  Buffer[11] = 0b10000001;
+  Buffer[12] = 0b10100101;
+  Buffer[13] = 0b10011001;
+  Buffer[14] = 0b01000010;
+  Buffer[15] = 0b00111100;
+
+  while(getControl() == NOINPUT)
+  {
+    if((getTime() - time_old) >= delay_time_scrolling)
+    {
+      i_x++;
+      scrollDisplay(0, 7, 1);
+      printChar(7 - i_x, 0, intro[i_char1]);
+      
+      if(i_x > 5)
+      {
+        i_x = 0;
+        if(intro[++i_char1] == '\0')
+        {
+          i_char1 = 0;
+        }
+      }
+  
+      displayLatch();
+      time_old = getTime();
+    }
+  }
+  displayClear();
+  displayLatch();
+  
+  i_char1 = 0;
 
   while(1)
   {
+    uint8_t key_pressed = getControl();
     
     switch(mode)
     {
       case PAUSE:
+        switch (key_pressed)
+        {
+          case ESCAPE:
+            displayClose();
+            return;
+          case LEFT:
+            mode = MENU;
+            i_char1 = 0;
+            i_char2 = 0;
+            displayClear();
+            displayLatch();
+            break;
+          case RIGHT:
+            break;
+          case UP:
+            restart(playground);
+            break;
+          case DOWN:
+            mode = PLAY;
+            break;
+        }
+
         if((getTime() - time_old) >= 400)
         { 
           if(pause_state)
@@ -395,24 +270,80 @@ void animateBadge(void)
 
             pause_state = 1;
           }
-          
+
+          displayLatch();
           time_old = getTime();
         }
         break;
 
       case PLAY:
+        switch (key_pressed)
+        {
+          case ESCAPE:
+            displayClose();
+            return;
+          case LEFT:
+            if(delay_time > 50)
+            {
+              delay_time -= 50;
+              displayClear();
+              printChar(2, 4, '-');
+              displayLatch();
+              time_old = getTime();
+            }
+            break;
+          case RIGHT:
+            delay_time += 50;
+            displayClear();
+            printChar(2, 4, '+');
+            displayLatch();
+            time_old = getTime();
+            break;
+          case UP:
+            restart(playground);
+            break;
+          case DOWN:
+            mode = PAUSE;
+            break;
+        }
+
         if((getTime() - time_old) >= delay_time)
         { 
           // show next generation
           refreshMatrix(playground);     
           // calculate next generation
           liveOrDie(playground);
-          
+
+
+          displayLatch();
           time_old = getTime();
         }
         break;
 
       case MENU:
+        switch (key_pressed)
+        {
+          case ESCAPE:
+            displayClose();
+            return;
+          case LEFT:
+            delay_time_scrolling -= 10;
+            break;
+          case RIGHT:
+            delay_time_scrolling += 10;
+            break;
+          case UP:
+            mode = PLAY;
+            restart(playground);
+            break;
+          case DOWN:
+            mode = EDITOR;
+            i_x = 0;
+            i_y = 0;
+            displayClear();
+            displayLatch();
+            break;
+        }
 
         if((getTime() - time_old) >= delay_time_scrolling)
         {
@@ -434,41 +365,128 @@ void animateBadge(void)
             }
           }
 
+          displayLatch();
           time_old = getTime();
         }
         break;
 
       case EDITOR:
 
-        displayPixel(i_x, i_y, 1);
-
-        if((i_x != x) || (i_y != y))
+        switch (key_pressed)
         {
-          if(!toggle_pixel)
-          {
-            displayPixel(x, y, 0);
-          }
-          toggle_pixel = 0;
+          case ESCAPE:
+            displayClose();
+            return;
+          case LEFT:
+            // toggle Pixel
+            if(readPixel(i_x, i_y, BufferEditor))
+            {
+              writePixel(i_x, i_y, 0, BufferEditor);
+            }
+            else
+            {
+              writePixel(i_x, i_y, 1, BufferEditor);
+            }
+            break;
+          case RIGHT:
+            if(++i_x == TOTPIXELX)
+            {
+              i_x = 0;
+            }
+            break;
+          case UP:
+            if(!i_x && !i_y)
+            {
+              refreshMatrix(BufferEditor);
+              displayLatch();
+              for(int i = 0; i < TOTPIXELY; i++)
+              {
+                playground[i] = BufferEditor[i];
+              }
+              mode = PLAY_EDITOR;
+            }
+            else if(i_y)
+            {
+              i_y--;
+            }
+            break;
+          case DOWN:
+            if(i_y < (TOTPIXELY - 1))
+            {
+              i_y++;
+            }
+            break;
         }
-        displayLatch();
 
-        if(i_y == -1)
-        {
-          i_x = 0;
-          i_y = 0;
+        if((getTime() - time_old) >= 100)
+        { 
+          refreshMatrix(BufferEditor);
 
-          for(int i = 0; i < TOTPIXELY; i++)
+          if(!blink_editor)
           {
-            BufferBackup[i] = Buffer[i];
-            playground[i] = Buffer[i];
+            displayPixel(i_x, i_y, 1);
           }
-          mode = PLAY_EDITOR;
+          else if((blink_editor == 1) && !readPixel(i_x, i_y, BufferEditor))
+          {
+            displayPixel(i_x, i_y, 0); 
+          }
+          else if (blink_editor == 3)
+          {
+            displayPixel(i_x, i_y, 0);
+          }
+          blink_editor++;
+
+          if(blink_editor >= 4)
+          {
+            blink_editor = 0;
+          }
+
+          displayLatch();
+          
+          time_old = getTime();
         }
-        x=i_x;
-        y=i_y;
         break;
 
       case PLAY_EDITOR:
+
+        switch (key_pressed)
+        {
+          case ESCAPE:
+            displayClose();
+            return;
+          case LEFT:
+            if(delay_time > 50)
+            {
+              delay_time -= 50;
+              displayClear();
+              printChar(2, 4, '-');
+              displayLatch();
+              time_old = getTime();
+            }
+            break;
+          case RIGHT:
+            delay_time += 50;
+            displayClear();
+            printChar(2, 4, '+');
+            displayLatch();
+            time_old = getTime();
+            break;
+          case UP:
+            displayClear();
+            displayLatch();
+            for(int i = 0; i < TOTPIXELY; i++)
+            {
+              Buffer[i] = BufferEditor[i];
+            }
+            mode = EDITOR;
+            i_x = 0;
+            i_y = 0;
+            break;
+          case DOWN:
+            mode = PAUSE_EDITOR;
+            break;
+        }
+
         if((getTime() - time_old) >= delay_time)
         { 
           // show next generation
@@ -476,11 +494,43 @@ void animateBadge(void)
           // calculate next generation
           liveOrDie(playground);
           
+          displayLatch();
           time_old = getTime();
         }
         break;
 
       case PAUSE_EDITOR:
+
+        switch (key_pressed)
+        {
+          case ESCAPE:
+            displayClose();
+            return;
+          case LEFT:
+            mode = MENU;
+            i_char1 = 0;
+            i_char2 = 0;
+            displayClear();
+            displayLatch();
+            break;
+          case RIGHT:
+            break;
+          case UP:
+            displayClear();
+            displayLatch();
+            for(int i = 0; i < TOTPIXELY; i++)
+            {
+              Buffer[i] = BufferEditor[i];
+            }
+            mode = EDITOR;
+            i_x = 0;
+            i_y = 0;
+            break;
+          case DOWN:
+            mode = PLAY_EDITOR;
+            break;
+        }
+
         if((getTime() - time_old) >= 400)
         { 
           if(pause_state)
@@ -498,19 +548,9 @@ void animateBadge(void)
             pause_state = 1;
           }
           
+          displayLatch();
           time_old = getTime();
         }
-        break;
-    }
-
-    switch(checkButtons())
-    {
-      case 1:
-        displayClose();
-        return;
-        break;
-      case 2:
-        restart(playground);
         break;
     }
   }
