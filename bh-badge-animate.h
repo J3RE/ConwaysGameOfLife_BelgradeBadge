20911ae6f4f7f79e60d59b58c36ad0774d623217
J3RE <j3re@gmx.at>
2016-03-27 00:59:20 +0100

    fixed prototypes

--- bh-badge-animate.h
+++ bh-badge-animate.h
@@ -4,9 +4,9 @@
 uint8_t readPixel(int8_t x, int8_t y, uint8_t array[]);
 void writePixel(uint8_t x, uint8_t y, uint8_t live, uint8_t array[]);
 uint8_t countNeighbours(int8_t x, int8_t y);
-void liveOrDie();
-void restart();
-void refreshMatrix();
+void liveOrDie(uint8_t array[]);
+void restart(uint8_t array[]);
+void refreshMatrix(uint8_t array[]);
 void printChar(int8_t x, int8_t y, char character);
 void scrollDisplay(uint8_t y_start, uint8_t y_end, int8_t steps);
 
